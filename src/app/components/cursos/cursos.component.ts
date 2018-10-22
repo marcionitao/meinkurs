2cef295785a6268503776b0d80d002905efda4a0
Marcio Nitao <marcionitao@gmail.com>
2018-10-22 20:16:17 +0200

    list cursos

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -10,10 +10,30 @@ import { CursoService } from '../../services/curso.service'
 })
 export class CursosComponent implements OnInit {
 
+  cursos: Curso[];
+  editState: boolean = false;
+  cursoToEdit: Curso;
+
   constructor(private service: CursoService) { }
 
   ngOnInit() {
-    this.service.getCursos().subscribe(cursos => console.log(cursos));
+    this.service.getCursos().subscribe(cursos => {this.cursos = cursos; console.log(this.cursos)});
   }
+  // Best practices for *ngFor
+  trackByFn(index, curso) {
+    return curso.id; // unique id corresponding to the item
+ }
+
+ clearState() {
+   this.editState = false;
+   this.cursoToEdit = null;
+ }
+
+ editCurso(event, curso: Curso) {
+  this.editState = true;
+  this.cursoToEdit = curso;
+ }
+
+ // Pause 9:03 - video 4
 
 }
